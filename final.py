fd2d0f375cb8fc9bcb2584463e2ae457feb27b72
MUHAMMED SHAHIL T <102034054+mst803@users.noreply.github.com>
2024-04-02 19:20:43 +0530

    Update final.py

--- final.py
+++ final.py
@@ -230,7 +230,7 @@ def main():
         if all_out:
             break
         for k in range(1,7):
-            if (LF_1.predict_proba([[inn,j,k,stadium,toss,on_field.peek(),bowling_2[j],Team_1,Team_2,Year]])[:,1])*r_f*r_f_o>0.337:
+            if (LF_1.predict_proba([[inn,j,k,stadium,toss,on_field.peek(),bowling_2[j],Team_1,Team_2,Year]])[:,1])*r_f*r_f_o>0.3:
                 r_f=0.8
                 r_f_o-=0.1
                 a=on_field.dequeue()
@@ -339,7 +339,7 @@ def main():
         if all_out:
             break
         for k in range(1,7):
-            if (LF_1.predict_proba([[inn,j,k,stadium,toss,on_field.peek(),bowling_1[j],Team_2,Team_1,Year]])[:,1])*r_f*r_f_o>0.337:
+            if (LF_1.predict_proba([[inn,j,k,stadium,toss,on_field.peek(),bowling_1[j],Team_2,Team_1,Year]])[:,1])*r_f*r_f_o>0.3:
                 r_f=0.8
                 r_f_o-=0.1
                 a=on_field.dequeue()
